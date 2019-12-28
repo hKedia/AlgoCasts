d8c0033c855b713aa834f7eeeb318b48ea274127
Harsh Kedia <harshkedia@outlook.in>
2019-12-28 22:58:47 +0530

    changing number to string

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,25 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    return Math.sign(n) * parseInt(n.toString().split('').reverse().join(''));
+}
+
+// function reverseInt(n) {
+//     let rev=0;
+//     let flag = false;
+//     if (n < 0) {
+//         flag = true;
+//         n = Math.abs(n);
+//     }
+//     while(n>0) {
+//         rev = (rev * 10) + n%10;
+//         n = Math.floor(n/10);
+//     }
+//     if(flag)
+//         return -rev;
+//     else
+//         return rev;
+// }
 
 module.exports = reverseInt;
