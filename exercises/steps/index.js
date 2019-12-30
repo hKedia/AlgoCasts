eff2f639e641cb5d2dffade65ec7c0e710c6070c
Harsh Kedia <harshkedia@outlook.in>
2019-12-30 23:21:29 +0530

    steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for (let i = 0; i<n; i++) {
+//         let str = '';
+//         for (let j=0; j<n ; j++) {
+            
+//             if (j<=i) {
+//                 str += '#';
+//             } else {
+//                 str += ' ';
+//             }
+//         }
+//         console.log(str);
+//     }
+// }
+
+
+function steps(n, row = 0, stair = '') {
+    if ( n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair);
+        steps(n, row + 1);
+        return;
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+    steps(n, row, stair);
+}
 
 module.exports = steps;
